701852088f82eb7c4193213e49195e9c372fc423
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-15 21:48:37 +0600

    all variables types

---
+++ variable.js
@@ -0,0 +1,33 @@
+// number type variable
+var sunglassPrice = 120;
+
+// String type variable
+var specialOneName = "Ariana Grande";
+
+// Boolean type variable
+var isSingle = true;
+
+// two types of number : Integer : 120,
+// float, decimal : 3.65
+
+
+// variable naming convention: rules, best practice
+
+/**
+ *  1. single word
+ *  2. No gap or space
+ *  3. no quote
+ *  4. no keyword
+ *  5. can not start with a number
+ *  6. can not contain dash
+ *  7. should use camelCase
+ */
+
+// Math Operations: +, -, *, /
+// ShorthHand: +=, -=, ++, --
+
+// Convert : praseInt, parseFloat
+
+// toFixed
+
+// reaminder: vagsesh : %
