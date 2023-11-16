5ecc53a0a0d15736e73b2186a3b8f8a4e0859ddd
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-16 22:59:27 +0600

    added array process

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFee = [5000, 2000,];
+var actress = ['Mahi', 'Opu','Sabnur', 'Shabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+//console.log(vowels);
+console.log(picnicFee.length); // .length deyar mane array koyta elements ase
