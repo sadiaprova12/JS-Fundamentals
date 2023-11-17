d5c2bf4e5f113484f979f735231fb47e9f1ce8eb
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-17 23:49:10 +0600

    added pop element

--- push-pop.js
+++ push-pop.js
@@ -1,6 +1,17 @@
 var numbers = [78, 45, 98, 45];
-numbers.push(63);
+// 1. Use push to add element to an array as the last element of the array
+//numbers.push(63);
 //console.log(numbers);
  var friends = ['Nabiha', 'Anika', 'Nisha'];
  friends.push('Moutorshi');
- console.log(friends);
+ friends.push('Monira');
+ //console.log(friends);
+
+ // 2. Use pop to get last element
+ console.log(numbers);
+ //numbers.pop();
+
+ // 3. Use pop to get last element
+ var element = numbers.pop();
+ console.log(numbers);
+ console.log(element);
