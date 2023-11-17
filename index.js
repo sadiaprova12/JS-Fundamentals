81c281fc47be6da4a651dd56b5fa0c0ca548bdd5
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-17 20:45:31 +0600

    added index change

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var numbers = [45, 68, 78, 56, 89, 98 ];
 // get element by index 
  var element = numbers[1];
  // console.log(element);
- numbers[1] = 77;
+ numbers[1] = 77; // ai namok index k set koro array 1 on positon maan
+ numbers[3] = 44;
  console.log(numbers);
