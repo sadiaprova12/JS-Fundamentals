f19b32d42d0daedca727913c67e96079fa515920
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-17 20:50:37 +0600

    Added find index element

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var numbers = [45, 68, 78, 56, 89, 98 ];
 
-// get element by index 
+// 1. Get element by index 
  var element = numbers[1];
  // console.log(element);
+
+// 2. Set element value by index 
  numbers[1] = 77; // ai namok index k set koro array 1 on positon maan
  numbers[3] = 44;
- console.log(numbers);
+ //console.log(numbers);
+
+ // 3. Find index of an element
+ var positionIndex = numbers.indexOf(89);
+ // var positionIndex = numbers.indexOf(189); // array moddhe nai
+ console.log(positionIndex);
+
