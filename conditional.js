762767cdc3db6561b67e5630b73767d832703a09
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-18 22:57:07 +0600

    added else conditon

--- conditional.js
+++ conditional.js
@@ -17,3 +17,26 @@ var myBudget = 95000;
 if (iphonePrice < myBudget){
     console.log('I will buy the iphone??');
 }
+
+ var chickenPrice = 500;
+ var myMoney = 50;
+// if (chickenPrice <= myMoney){
+//     console.log('Cock Chicken');
+// }
+
+// if (chickenPrice >= myMoney){
+//     console.log('Smashed potato and lentils soup');
+// }
+
+/**
+ *  if chicken er price is less than my budget
+ *  I will eat chicken
+ *  na hoi: else
+ */
+
+if(chickenPrice < myBudget){
+    console.log('I will eat chicken');
+}
+else{
+    console.log('I will eat potato');
+}
