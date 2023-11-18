f0ba18998e65ffb48b892d9467b9dec14bbd754a
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-18 22:17:28 +0600

    added not equal comparison

--- comparison.js
+++ comparison.js
@@ -1,4 +1,15 @@
+// 1. [Grather than]
 //console.log(5 < 6);
+
+
+// 2. [ Less than ]
 //console.log(5 > 6);
+
+// 3. [5 er 6 r equal kina? ]
 //console.log(5 == 6);
-console.log(6 == 6);
+
+// 4. [6 er 6 er equal kina? ]
+//console.log(6 == 6);
+
+// 5. [5 er 6 er not equal kina?]
+console.log(5 != 6);
