0f51e77312e62f12e665dcce989382b6c8ba813e
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-18 22:48:02 +0600

    added if condition

---
+++ conditional.js
@@ -0,0 +1,19 @@
+var iphonePrice = 79000;
+//var myBudget = 9500;
+var myBudget = 95000;
+// 1. [ if iphone price is less than my budget. i will buy the iphone]
+
+/**
+ *  if iphone price is less than my budget
+ *  i will buy the iphone
+ * 
+ * 
+ * 
+ *  if (condition){
+ *   // will execute if the condition is true
+ * }
+ */
+
+if (iphonePrice < myBudget){
+    console.log('I will buy the iphone??');
+}
