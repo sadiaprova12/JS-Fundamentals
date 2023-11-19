baffe4842c1c526f4f7b184f903918eb0471794c
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 17:50:31 +0600

    added summary

---
+++ summary.js
@@ -0,0 +1,29 @@
+// // Array declare 
+// var destinations = ['Shajek', 'Coxs Bazar', 'Nepal', 'Bali', 'SP'];
+
+// //console.log(destinations[2]);
+// //
+// destinations[2]= 'Thailand';
+// var index = destinations.indexOf('Bali');
+
+// destinations.push('Bhutan');
+// destinations.pop();
+
+// // Conditionals
+// /**
+//  * <, >, ==, !=, <=, >=
+//  *  &&, ||
+//  */
+
+// if(destinations[1] == 'Shajek'){
+
+// }
+// else if (destinations[2] == 'Bali'){
+
+// }
+// else if(destinations.length !== 3){
+
+// }
+// else{
+
+// }
