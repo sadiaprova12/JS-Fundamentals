e15e987c68fc0f164d83cfc87dc2c264a387dd04
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 17:39:10 +0600

    added nested condition

---
+++ nested-conditions.js
@@ -0,0 +1,16 @@
+var math = true;
+var geometry = true;
+var straighthLint = false;
+if(math == true){
+    if(geometry == true){
+    if(straighthLint == true){
+
+    }
+    else{
+        console.log('Baka pothe chola jabe na');
+    }
+    }
+    else{
+        console.log('Not a Pethagrous');
+    }
+}
