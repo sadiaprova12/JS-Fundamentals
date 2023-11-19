6afc3d7916471bc9f162151ae5c1209227b7d8c3
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 17:06:44 +0600

    added 2 condition true process

--- multiple-condition.js
+++ multiple-condition.js
@@ -1,7 +1,7 @@
-var isGraduated = true;
-// var salary = 25000;
-var salary = 75000;
-var cars = 1;
+// var isGraduated = true;
+// // var salary = 25000;
+// var salary = 75000;
+// var cars = 1;
 // if(isGraduated == true){
 //     console.log('Apply to Job')
 // }
@@ -10,9 +10,26 @@ var cars = 1;
 //     console.log('Nothing!!')
 // }
 
-if(isGraduated == true && salary > 50000 && cars >= 1){
-    console.log('Apply job')
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('Apply job')
+// }
+// else{
+//     console.log('Nothing!!')
+// }
+
+
+var isGraduated = true;
+var salary = 31000;
+var cars = 0;
+// j kuno 1 ta true hole hbe
+if(isGraduated == true || salary > 50000){
+    //console.log('Come here');
 }
 else{
-    console.log('Nothing!!')
+    //console.log('Get lost');
+}
+
+// 2 ta true hote hbe
+if((isGraduated == true && salary > 50000) || cars >=1){
+       
 }
