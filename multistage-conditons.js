603b8c3674d24ac68a0a8918c4f1f1cc1c0e8060
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 17:30:57 +0600

    added multi stage conditons

---
+++ multistage-conditons.js
@@ -0,0 +1,17 @@
+var money = 40;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if(danishPrice < money){
+    console.log('Get buy Danish');
+}
+else if (butterBread < money){
+    console.log('Get Buy Butter Bread');
+}
+else if (toastBiscuit < money){
+    console.log('Get Buy Toast Biscuit');
+}
+else{
+    console.log('Nothing,Money!!');
+}
