633b6fb9596edae7ce851574e025aedfdd13c96c
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 18:08:36 +0600

    added problem 1

---
+++ task.js
@@ -0,0 +1,18 @@
+// Problem 1
+
+var fruits = ['Apple', 'Banana', 'Orange']
+// a. Find the index of 'Banana'
+var element = fruits[1];
+//console.log(element);
+
+// b. Replace 'Banana' with 'Mango'
+fruits[1] = 'Mango';
+//console.log(fruits);
+
+// Last element remove
+fruits.pop();
+console.log(fruits);
+
+// added element
+fruits.push('Watermelone');
+console.log(fruits);
