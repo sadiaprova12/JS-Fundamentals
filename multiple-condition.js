bb3db04eee26c20d41938513f57a43749ad7f9e5
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 16:53:23 +0600

    added multiple condition

---
+++ multiple-condition.js
@@ -0,0 +1,18 @@
+var isGraduated = true;
+// var salary = 25000;
+var salary = 75000;
+var cars = 1;
+// if(isGraduated == true){
+//     console.log('Apply to Job')
+// }
+// // Eka dhikh condition
+// else{
+//     console.log('Nothing!!')
+// }
+
+if(isGraduated == true && salary > 50000 && cars >= 1){
+    console.log('Apply job')
+}
+else{
+    console.log('Nothing!!')
+}
